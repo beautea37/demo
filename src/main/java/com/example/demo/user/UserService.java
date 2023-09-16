4db4ddf2f748cf07913a56316fdc6c3e0e592507
beautea37 <beautea37@gmail.com>
2023-09-16 20:18:22 +0900

    게시판 복습2

--- src/main/java/com/example/demo/user/UserService.java
+++ src/main/java/com/example/demo/user/UserService.java
@@ -32,5 +32,4 @@ public class UserService {
             throw new DataNotFoundException("사용자 정보를 찾을 수 없습니다.");
         }
     }
-
 }
--- src/test/java/com/example/demo/QuestionTests.java
+++ src/test/java/com/example/demo/QuestionTests.java
@@ -15,15 +15,16 @@ import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+
+// 전제 코드 리팩토링 중 문제 없는 걸로 보임.
 @SpringBootTest
 @AutoConfigureMockMvc
 public class QuestionTests {
     @Autowired
     private MockMvc mockMvc;
 
-
-
     //QuestionForm TestCode
+    //#확인요망. 이 부분 테스트코드가 애매하게 실행되어 있음. 예외인 부분.
     @Test
     void questionFormValidationTest() {
         Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
@@ -34,14 +35,14 @@ public class QuestionTests {
         invalidFormA.setContent("Some content");
 
         Set<ConstraintViolation<QuestionForm>> violationsA = validator.validate(invalidFormA);
-        assertThat(violationsA).isNotEmpty();
+        assertThat(violationsA).isNotEmpty(); // #문제 없음: 유효하지 않은 케이스를 검사하는 것이므로 문제 없음.
 
         QuestionForm invalidFormB = new QuestionForm();
         invalidFormB.setSubject("A valid subject");
         invalidFormB.setContent("");
 
         Set<ConstraintViolation<QuestionForm>> violationsB = validator.validate(invalidFormB);
-        assertThat(violationsB).isNotEmpty();
+        assertThat(violationsB).isNotEmpty(); // #문제 없음: 유효하지 않은 케이스를 검사하는 것이므로 문제 없음.
 
         // 일부 유효한 케이스
         QuestionForm validForm = new QuestionForm();
@@ -49,7 +50,6 @@ public class QuestionTests {
         validForm.setContent("This is a valid content");
 
         Set<ConstraintViolation<QuestionForm>> validViolations = validator.validate(validForm);
-        assertThat(validViolations).isEmpty();
+        assertThat(validViolations).isEmpty(); // #문제 없음: 유효한 케이스를 검사하므로 문제 없음.
     }
-
 }
